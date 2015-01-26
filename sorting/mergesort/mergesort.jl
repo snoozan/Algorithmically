5e01eabee5b99af0be4ef158b26e676d6de87ffb
Susan Lunn <slgems@gmail.com>
2015-01-26 09:45:18 -0500

    started julia mergesort

---
+++ sorting/mergesort/mergesort.jl
@@ -0,0 +1,13 @@
+#Mergesort in Julia! 
+
+#set this to always be an Int
+function split(arr)
+    if(size(arr, x) < 2)
+        return arr
+    middle = size(arr, x)//2
+end
+
+
+
+arr = [5,6,7]
+split(arr)
